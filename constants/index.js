11d0783d3ddc18c783399e27fd70153a76fa0687
theDMancer <130903007+theDMancer@users.noreply.github.com>
2023-07-30 04:24:43 -0930

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x4F52CB0dacd997b0CE43eeE93caAFDD337aAcE8e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
